cc9f83ade11f28a667d9f4e666f8ee77a642a602
Khoa Tran <duykhoa12t@gmail.com>
2013-08-04 22:04:54 +0700

    Fix rails version

--- config/application.rb
+++ config/application.rb
@@ -35,7 +35,7 @@ module Rottenpotatoes
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
     # config.time_zone = 'Central Time (US & Canada)'
 
-    # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
+    # The default locale is :en an =d all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
 
@@ -50,5 +50,7 @@ module Rottenpotatoes
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    config.assets.initialize_on_precompile = false
   end
 end
